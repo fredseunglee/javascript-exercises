e840579023f037db9ad3e48c2b8569e396771920
Fred Lee <164126448+fredseunglee@users.noreply.github.com>
2024-04-03 03:15:13 -0400

    complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (repeatString, repeatCount) {
+  if (repeatCount < 0) {
+    return "ERROR";
+  }
+  let finalString = "";
+  for (let i = 0; i < repeatCount; i++) {
+    finalString += repeatString;
+  }
 
+  return finalString;
 };
 
 // Do not edit below this line
