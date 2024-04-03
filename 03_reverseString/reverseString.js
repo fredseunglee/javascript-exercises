65ddc2ab0d58b04d7a9820060ca76f99b8141d13
Fred Lee <164126448+fredseunglee@users.noreply.github.com>
2024-04-03 03:24:33 -0400

    finish exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (stringToReverse) {
+  let finalString = "";
+  for (let i = stringToReverse.length - 1; i >= 0; i--) {
+    finalString += stringToReverse.at(i);
+  }
+  //return finalString;
+  return stringToReverse.split("").reverse().join("");
 };
 
 // Do not edit below this line
