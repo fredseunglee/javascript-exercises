259410a3664779a0a80c3cf8f14dfa095ed9476b
Fred Lee <164126448+fredseunglee@users.noreply.github.com>
2024-04-03 04:36:48 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    num1 < 0 ||
+    typeof num2 !== "number" ||
+    num2 < 0
+  ) {
+    return "ERROR";
+  }
 
+  if (num2 < num1) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+
+  let finalNum = 0;
+  for (let i = num1; i <= num2; i++) {
+    finalNum += i;
+  }
+  return finalNum;
 };
 
 // Do not edit below this line
